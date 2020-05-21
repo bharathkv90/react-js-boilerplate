7a27d4c593f033923cd59508c85b730015ef3c76
Bharath Kumar K V <bharath.kv@redbus.com>
2020-05-21 22:32:56 +0530

    react setup with babel and webpack

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/styles/styles.css';
+
+const jsx = (
+    <div>
+        <h1>Hello World</h1>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById("root"));
